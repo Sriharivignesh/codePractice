9aad13cfe125057b7d318b9e0283edd49d518f32
Sriharivignesh <srihari8@uw.edu>
2016-06-23 17:56:19 +0530

    Reverse Polish Notation Evaluation - Codefights

---
+++ Python/Reverse_Polish_Notation_Evaluation.py
@@ -0,0 +1,52 @@
+def RPN(e):
+    n = 0
+    r = []
+    k = 0
+    a = 0
+    b = 0
+    for i in range(len(e)):
+        if(e[i].isspace()):
+            if(k == 1):
+                r.append(n)
+            n = 0
+        elif(e[i] == '+'):
+            b = r.pop()
+            a = r.pop()
+            r.append(a + b)
+            k = 0
+        elif(e[i] == '-'):
+            b = r.pop()
+            a = r.pop()
+            r.append(a - b)
+            k = 0
+        elif(e[i] == '*'):
+            b = r.pop()
+            a = r.pop()
+            r.append(a * b)
+            k = 0
+        elif(e[i] == '/'):
+            b = r.pop()
+            a = r.pop()
+            r.append(a / b)
+            k = 0
+        elif(e[i] == '%'):
+            b = r.pop()
+            a = r.pop()
+            r.append(a % b)
+            k = 0
+        elif(e[i] == '^'):
+            b = r.pop()
+            a = r.pop()
+            r.append(a**b)
+            k = 0
+        else:
+            n = n*10;
+            n = n + (int)(e[i])
+            k = 1
+
+    return r.pop()
+
+print(RPN("4 0 3 - -"))
+
+
+
