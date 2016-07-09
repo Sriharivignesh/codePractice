54dcf90a399fd70e94dbded12ea881c670328d78
Sriharivignesh <srihari8@uw.edu>
2016-07-09 19:56:08 +0530

    Find Bi-Slice - Codefights

--- C++/FindBiSlice.cpp
+++ C++/FindBiSlice.cpp
@@ -14,7 +14,7 @@
 /* Keeps inserting numbers in std::set. When set size exceeds 2, 
 ** it means 3 different numbers are in the set which resets the count of the longest contiguous array.
 */
-int findBiSlice(std::vector<int> arr) {
+/*int findBiSlice(std::vector<int> arr) {
     int maximum = -1000;
     int count = 0;
     int size = arr.size(); //Size of input vector 'arr'
@@ -41,6 +41,76 @@ int findBiSlice(std::vector<int> arr) {
         }
     }
     return maximum;
+}*/
+
+//FOLLOWING IS THE IMPLEMENTATION OF O(N) solution suggested by Sir_Ementaler which works wonderfully - how could I not have thought if this earlier??
+
+/* Iterate over the array a while keeping the last two encountered values in mind, say x and y. If a[i] is either x or y, then the calculated length of the sequence of x's and y's is incremented. If it some z that is neither x nor y, then the sequence of x's and y's is closed and we start counting ** a new sequence. This new sequence is a sequence of either x's and z's or y's and z's, depending on whether an occurrence of x or y was more recent, and already has some entries, i.e. the number of uninterrupted occurrences of x or y prior to encountering z. That means you also need to remember ** which of the values was more recent, and how many times in a row it last occurred. The returned value is the maximum found length of any sequence.
+** You can see my solution for one possible implementation. It's very obfuscated but readable if you replace conditional operators with ifs and elses (I also use a ^ b in place of a != b). Variable descriptions:
+** x - most recent found value
+** y - second most recent found value
+** s - length of the ongoing sequence of xs
+** t - length of the ongoing sequence of xs and ys
+** r - greatest length found so far
+*/
+
+int findBiSlice(std::vector<int> arr) { //{5, 8, 8, 6, 7, 4, 3, 4, 3}
+    int x = arr[0];
+    int y = arr[1];
+    int x_count = 0;
+    int y_count = 0;
+    int count = 0;
+    int last = 0;
+    int maximum = -100;
+
+    for(int i = 0; i < arr.size(); i++)
+    {
+        if(arr[i] == x)
+        {
+            count++;
+            x_count++;
+            last = x;
+        }
+        else if(arr[i] == y)
+        {
+            count++;
+            y_count++;
+            last = y;
+        }
+        else
+        {
+            if(last == x)
+            {
+                if(count > maximum)
+                {
+                    maximum = count;
+                }
+                count = x_count + 1;
+                y = arr[i];
+                last = y;
+                y_count = 1;
+            }
+            else
+            {
+                if(count > maximum)
+                {
+                    maximum = count;
+                }
+                count = y_count + 1;
+                x = arr[i];
+                last = x;
+                x_count = 1;
+            }
+        }
+    }
+        if(count > maximum)
+        {
+            return count;
+        }
+        else
+        {
+            return maximum;
+        }
 }
 
 
@@ -66,7 +136,7 @@ int findBiSlice(std::vector<int> arr) {
 
 int main()
 {
-    std::vector<int> input = {5, 8, 8, 6, 7, 4, 3, 4, 3};
+    std::vector<int> input = {4, 5, 5, 4, 4, 5};
     std::cout<<findBiSlice(input)<<std::endl;
     return 0;
 }
