dd110e0fa28c2162a47de20eb826d53d32b07825
Sriharivignesh <srihari8@uw.edu>
2016-09-23 21:17:42 -0700

    Prepended std:: to a few lines

--- C++/sparse_arrays.cpp
+++ C++/sparse_arrays.cpp
@@ -12,21 +12,21 @@ using namespace std;
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
     int number = 0;
-    cin>>number;
+    std::cin>>number;
     string input_string;
     unordered_map <string,int> string_count;
     for(int i = 0;i < number;i++)
     {
-        cin>>input_string;
+        std::cin>>input_string;
         //cout<<string_count.at(input_string);
         //string_count.insert(make_pair(input_string,string_count[input_string]++));
         string_count[input_string]++;
     }
-    cin>>number;
+    std::cin>>number;
     for(int i = 0;i < number; i++)
     {
-        cin>>input_string;
-        cout<<string_count[input_string]<<endl;
+        std::cin>>input_string;
+        std::cout<<string_count[input_string]<<endl;
     } 
       
     return 0;
