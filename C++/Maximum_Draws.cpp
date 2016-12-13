7f1bc176f1a764fc4e5b19152afdfd664bd97c99
Sriharivignesh <srihari8@uw.edu>
2016-12-13 09:21:41 -0800

    Maximum Draws - Hackerrank

---
+++ C++/Maximum_Draws.cpp
@@ -0,0 +1,21 @@
+//https://www.hackerrank.com/challenges/maximum-draws?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int n = 0;
+    std::cin >> n;
+    while(n--)
+        {
+        int k = 0;
+        std::cin >> k;
+        std::cout << (k + 1) << "\n";
+    }
+    return 0;
+}
