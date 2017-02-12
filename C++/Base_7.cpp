b7f0a84adb20e7d4256592d51eab10764415f894
Sriharivignesh <srihari8@uw.edu>
2017-02-12 00:43:12 -0800

    Base 7 - Leetcode

---
+++ C++/Base_7.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <string>
+#include <vector>
+#include <limits>
+#include <stack>
+#include <iterator>
+#include <set>
+#include <queue>
+#include <sstream>
+#include <stdlib.h>
+#include <fstream>
+#include <cstdint>
+#include <random>
+#include <valarray>
+#include <numeric>
+#include <sstream>
+
+class Solution {
+public:
+    std::string convertTo7(int num) {
+        bool negative = 0;
+        if(num < 0)
+        {
+            negative = 1;
+            num = -1 * num;
+        }
+        std::stack<int> keeper;
+        while(num)
+        {
+            keeper.push(num%7);
+            num /= 7;
+        }
+        int temp = 0;
+        while(!keeper.empty())
+        {
+            temp *= 10;
+            temp += keeper.top();
+            keeper.pop();
+        }
+        return std::to_string(negative?(-1 * temp):temp);
+        
+    }
+};
+
+int main()
+{
+    Solution s1;
+    std::cout << s1.convertTo7(10) << "\n";
+    return 0;
+}
