4b3605301bd0f77cf0dedd977aed0146b38bfb3d
Sriharivignesh <srihari8@uw.edu>
2017-02-11 17:25:54 -0800

    Google foobar challenge 1

---
+++ Python/google.py
@@ -0,0 +1,20 @@
+import math
+
+def answer(area):
+    # your code here
+    result_list = []
+    lookup = []
+
+    while(area > 0):
+        rough_side = math.sqrt(area)
+        floor_rough_side = int(rough_side)
+        print(floor_rough_side)
+        print(floor_rough_side ** 2)
+        result_list.append(floor_rough_side ** 2)
+        area -= floor_rough_side ** 2
+        
+    return result_list
+
+
+printing = answer(12)
+print(printing)
